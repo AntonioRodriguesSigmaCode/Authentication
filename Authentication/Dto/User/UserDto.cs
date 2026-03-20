dcb5ea205934e89d4b1fdc8a5481b66d15fe26ec
AntonioRodriguesSigmaCode <antonio.rodrigues@sigmacode.pt>
2026-03-20 12:44:43 +0000

    Removed

--- Authentication/Dto/User/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Authentication.Dto.User
-{
-	public class UserDto
-	{
-		public int Id { get; set; }
-		public string Username { get; set; }
-		public List<RoleDto> Roles { get; set; }
-	}
-}
